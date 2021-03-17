860b7a47a612b0453cee703b5a003ea8805791d6
Michael Mayer <michaelmayer171@gmail.com>
2021-03-16 19:31:47 -0500

    initial commit with general app structure and README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Recipe Book App
+
+The Recipe Book App allows users to easily aggregate recipies found across the web and store them in one place while eliminating the "fluff" found on most recipe websites.  Users will also be able to input thier own recipies for safe keeping. (Stretch goal of incoprorating Tasty API)
+
+## Stack 
+- Express
+- Node.js
+- Mongoose
+- EJS
+- CSS Framework???
+
+## User Story
+- New Users will create an account for return logins 
+- Users will be able to add recipies by copying recipe text from websites or input thier own reciepies
+- Recipe tags can be added for easy searching 
+- Recipies can be edited and updated
+- Recipies can versioned (duplicated then edited) to allow for recipe experimentation 
+- Users have the ability "Favorite" recipies for display on user homepage 
+
+## Wireframe 
+![wireframe](public/css/Images/IMG_0759.png)
+
+
---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+const session = require('express-session');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+
+const mongoURI = process.env.MONGODBURI
+const db = mongoose.connection;
+
+mongoose.connect(mongoURI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, ()=>{
+    console.log("database connection checked");
+})
+
+db.on('error', (err)=> { console.log('ERROR: ', err)});
+db.on('connected', ()=> { console.log("mongo connected")})
+db.on('disconnected', ()=> { console.log("mongo disconnected")})
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secrete: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+const isAuthenticated = (req, res, next) => {
+    if (req.session.currentUser) {
+        return next()
+    } else {
+        res.redirect('/sessions/new')
+    }
+}
+
+
+//Controllers go here 
+
+
+const userControllers = require('./controllers/users')
+app.use('/users', isAuthenticated, userControllers)
+
+const sessionsController = require('./controllers/sessions')
+app.use('/session', sessionsController)
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`)
+})
